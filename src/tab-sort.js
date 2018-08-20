86d421a7737eb27d0c7a2d3344a894eb16f2f887
krokowski0x <krokowski.dx@gmail.com>
2018-08-20 19:12:50 +0200

    Improved readability with Prettier

--- src/tab-sort.js
+++ src/tab-sort.js
@@ -2,7 +2,7 @@
 let domains = [];
 // Sort only current window tabs
 const params = {
-  currentWindow: true,
+	currentWindow: true
 };
 
 // Chrome extension event listener
@@ -10,31 +10,29 @@ chrome.browserAction.onClicked.addListener(sortingTriggered);
 
 // When sorting has been triggered
 sortingTriggered = () => {
-  // Clear previous container
-  domains = [];
-  // Rearrange tabs in current window
-  chrome.tabs.query(params, sortTabs);
-}
+	// Clear previous container
+	domains = [];
+	// Rearrange tabs in current window
+	chrome.tabs.query(params, sortTabs);
+};
 
-sortTabs = (tabs) => {
-  // For each tab
-  for (tab of tabs) {
-    // Parse it from url to domain name
-    const domain = {
-      domain: tab.url.replace(/^(https||http)\:\/\/(www\.||)(.*?)\..*/g,'$3'),
-      isPDF: (/.pdf$/g).test(tab.url),
-      id: tab.id,
-    }
-    domains.push(domain);
-  }
-  // Sort domains
-  domains.sort((prev, next) => (prev.domain > next.domain));
-  // Rearrange tabs
-  for (const [index, domain] of domains.entries()) {
-    // If it's PDF, move it to the end
-    if (domain.isPDF)
-      chrome.tabs.move(domain.id, { index: -1 });
-    else
-      chrome.tabs.move(domain.id, { index });
-  }
-}
+sortTabs = tabs => {
+	// For each tab
+	for (tab of tabs) {
+		// Parse it from url to domain name
+		const domain = {
+			domain: tab.url.replace(/^(https||http)\:\/\/(www\.||)(.*?)\..*/g, "$3"),
+			isPDF: /.pdf$/g.test(tab.url),
+			id: tab.id
+		};
+		domains.push(domain);
+	}
+	// Sort domains
+	domains.sort((prev, next) => prev.domain > next.domain);
+	// Rearrange tabs
+	for (const [index, domain] of domains.entries()) {
+		// If it's PDF, move it to the end
+		if (domain.isPDF) chrome.tabs.move(domain.id, { index: -1 });
+		else chrome.tabs.move(domain.id, { index });
+	}
+};
